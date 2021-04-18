67cd944afee99307817008661afb838c156f9cec
Tobi <tobi.daniel2018@gmail.com>
2021-04-18 14:59:49 -0400

    commit

--- 3-useState-array.js
+++ 3-useState-array.js
@@ -1,15 +1,27 @@
 import React from "react";
 import { data } from "../../../data";
 
-
 const UseStateArray = () => {
   const [people, setPeople] = React.useState(data);
+
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+  };
   return (
     <>
       {people.map((person) => {
-        console.log(person);
-        return "hello";
+        const { id, name } = person;
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}>remove</button>
+          </div>
+        );
       })}
+      <button className="btn" onClick={() => setPeople([])}>
+        clear items
+      </button>
     </>
   );
 };
