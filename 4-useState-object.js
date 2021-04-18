b14f632640c227ce638e9f31b050a60f38cb79a1
Tobi <tobi.daniel2018@gmail.com>
2021-04-18 17:04:45 -0400

    learning useEffect

--- 4-useState-object.js
+++ 4-useState-object.js
@@ -1,20 +1,31 @@
 import React, { useState } from "react";
 
 const UseStateObject = () => {
-  const [person, setPerson] = useState({
-    name: "peter",
-    age: 24,
-    message: "random message",
-  });
+  // const [person, setPerson] = useState({
+  //   name: "peter",
+  //   age: 24,
+  //   message: "random message",
+  // });
+  // const changeMessage = () => {
+  //   setPerson({ ...person, message: "hello world" });};
+
+  // Working with Multiple State Values, you get the same result as above using an object
+  const [name, setName] = useState("peter");
+  const [age, setAge] = useState(24);
+  const [message, setMessage] = useState("random message");
+
   const changeMessage = () => {
-    setPerson({...person, message: 'hello world'})
-  }
+    setMessage("hello world");
+  };
+
   return (
     <>
-      <h3>{person.name}</h3>
-      <h3>{person.age}</h3>
-      <h3>{person.message}</h3>
-      <button className="btn" onClick={changeMessage}>change message</button>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{message}</h3>
+      <button className="btn" onClick={changeMessage}>
+        change message
+      </button>
     </>
   );
 };
